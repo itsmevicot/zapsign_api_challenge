d681ae18e7dd58c325b1da71c454f929a0032298
Victor Chagas <victorchagasms@gmail.com>
2024-12-07 16:32:18 -0300

    feat: updates handler

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,6 +1,9 @@
-from rest_framework.views import exception_handler
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError as DRFValidationError
+from rest_framework.response import Response
+from rest_framework.views import exception_handler
+from rest_framework_simplejwt.exceptions import TokenError
+
 from utils.exceptions import ExceptionMessageBuilder
 
 
@@ -19,8 +22,37 @@ def custom_exception_handler(exc, context):
             status=exc.status_code or status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
 
-    response = exception_handler(exc, context)
+    if isinstance(exc, DRFValidationError):
+        return Response(
+            {
+                "error": {
+                    "title": "Validation Error",
+                    "message": exc.detail,
+                }
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
+    if isinstance(exc, AttributeError):
+        return Response(
+            {
+                "error": {
+                    "title": "Server Error",
+                    "message": "An unexpected error occurred. Please try again later."
+                }
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    if isinstance(exc, TokenError):
+        return Response(
+            {
+                "message": "The refresh token has already been invalidated or is no longer valid."
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    response = exception_handler(exc, context)
     if response is not None:
         response.data["status_code"] = response.status_code
 
