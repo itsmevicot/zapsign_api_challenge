8620b6fd2d5fbc66e54f58e48e2e2d70a1c0d952
Victor Chagas <victorchagasms@gmail.com>
2024-12-07 16:30:58 -0300

    feat: adapt modeling to align with existing business rules and enable suggested flow

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Document(models.Model):
-    open_id = models.IntegerField(null=False)
-    token = models.CharField(max_length=255, null=False)
+    open_id = models.IntegerField(null=True)
+    token = models.CharField(max_length=255, null=True)
     name = models.CharField(max_length=255, null=False)
-    status = models.CharField(max_length=50, null=False)
+    status = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.CharField(max_length=255, null=False)
+    created_by = models.CharField(max_length=255, null=True)
     company = models.ForeignKey("companies.Company", on_delete=models.PROTECT, related_name='documents')
     external_id = models.CharField(max_length=255, null=True, blank=True)
 
